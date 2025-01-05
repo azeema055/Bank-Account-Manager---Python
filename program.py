dd5e0f1342aa76de62e7ca50ce92e5e9eacee982
Azeem <azeema055@gmail.com>
2025-01-05 18:20:47 +0500

    Deposit Function

--- program.py
+++ program.py
@@ -1,1 +1,25 @@
 #Bank Account Manager Program
+
+class Bankaccount:
+    def __init__(self, name, acc_number, balance = 0):
+        self.__name = name
+        self.__balance = balance
+        self.__acc_number = acc_number
+        self.transactios = []
+    def get_balance(self):
+        return self.__balance
+    
+    def deposit(self, amount):
+        try:
+            if amount <=0:
+                raise ValueError("Deposit amount must be positive.")
+            self.__balance += amount
+            self.transactios.append(f"Deposited: ${amount}")
+            print(f"${amount} deposited successfully!")
+        except ValueError as e:
+            print(f"Error: {e}")
+
+
+
+
+    
